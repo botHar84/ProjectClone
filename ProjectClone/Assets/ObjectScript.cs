81087e921acc67cc39cea483bf0620c01fc11472
MushyZ <kashinathr112@gmail.com>
2024-09-26 17:37:41 +0530

    SFX AND VFX

--- ProjectClone/Assets/ObjectScript.cs
+++ ProjectClone/Assets/ObjectScript.cs
@@ -14,6 +14,17 @@ public class ObjectScript : MonoBehaviour
     public ObjectScript subject;
     public Vector3 og;
     public Vector3 change;
+
+    [Header("SFX")]
+    public AudioClip spikeCeilingSFX;
+    public AudioClip platformMovingSFX;
+    public AudioClip pressurePlateUpSFX;
+    public AudioClip pressurePlateDownSFX;
+    public AudioClip leverOnSFX;
+    public AudioClip leverOffSFX;
+    public AudioClip doorOpenSFX;
+    public AudioClip timeCrystalSFX;
+
     public void Start()
     {
         if (name != "SpikeCeiling")
@@ -40,6 +51,7 @@ public class ObjectScript : MonoBehaviour
                 if (!on)
                 {
                     turnon();
+                    SoundFXManager.instance.PlaySoundFXClip(leverOnSFX, transform, 1f);
                     //subject.turnon();
                 }
             }
@@ -70,13 +82,26 @@ public class ObjectScript : MonoBehaviour
                 }
                 else if (type == "TimeCrystal")
                 {
-                    other.gameObject.GetComponent<PlayerScript>().reversed = true;
-                    other.gameObject.transform.Find("TimeParticles").GetComponent<ParticleSystem>().Play();
+                     // Create a unique key for this level's time crystal
+                    string timeCrystalKey = "TimeCrystalTouched_Level" + PlayerPrefs.GetInt("CurrentLevel");
+
+                    // Check if the player has already touched the crystal in this level
+                    if (PlayerPrefs.GetInt(timeCrystalKey, 0) == 0)
+                    {
+                        // First time touching the crystal in this level
+                        other.gameObject.GetComponent<PlayerScript>().reversed = true;
+                        other.gameObject.transform.Find("TimeParticles").GetComponent<ParticleSystem>().Play();
+                        SoundFXManager.instance.PlaySoundFXClip(timeCrystalSFX, transform, 1f);
+
+                        // Mark the time crystal as touched for this level
+                        PlayerPrefs.SetInt(timeCrystalKey, 1);
+                    }
                 }
             }
         }
         
     }
+
     void OnTriggerExit2D(Collider2D other)
     {
         if (type == "Plate" && (other.gameObject.tag == "Player" || other.gameObject.tag == "Clone"))
@@ -99,14 +124,19 @@ public class ObjectScript : MonoBehaviour
         if (type == "Lever" || type == "EndDoor")
         {
             gameObject.GetComponent<Animator>().SetTrigger("On");
+            SoundFXManager.instance.PlaySoundFXClip(doorOpenSFX, transform, 1f);
         }
         else if (type == "Plate")
         {
             sr.sprite = activated;
+            SoundFXManager.instance.PlaySoundFXClip(pressurePlateDownSFX, transform, 1f);
         }
         if (type == "Door")
         {
             GetComponent<BoxCollider2D>().enabled = false;
+        }
+        if (gameObject.tag == "SpikeCeiling"){
+            SoundFXManager.instance.PlaySoundFXClip(spikeCeilingSFX, transform, 1f);
         }
     }
     public void turnoff()
@@ -119,10 +149,16 @@ public class ObjectScript : MonoBehaviour
         if (name == "Lever" || name == "EndDoor")
         {
             gameObject.GetComponent<Animator>().SetTrigger("Off");
+            SoundFXManager.instance.PlaySoundFXClip(leverOffSFX, transform, 1f);
         }
         else if (name == "Plate")
         {
             sr.sprite = unactivated;
+            SoundFXManager.instance.PlaySoundFXClip(pressurePlateUpSFX, transform, 1f);
+            if (SoundFXManager.instance.IsPlaying(platformMovingSFX))
+            {
+                SoundFXManager.instance.StopSoundFXClip(platformMovingSFX);
+            }
         }
         if (type == "Door")
         {
@@ -135,6 +171,14 @@ public class ObjectScript : MonoBehaviour
         {
             while(on)
             {
+                if (!(gameObject.tag == "SpikeCeiling"))
+                {
+                    if (!SoundFXManager.instance.IsPlaying(platformMovingSFX))
+                    {
+                        SoundFXManager.instance.PlaySoundFXClip(platformMovingSFX, transform, 1f);
+                    }
+                }
+                
                 if (transform.position != og+change)
                 {
                     transform.position = Vector3.Lerp(transform.position, og+change, amnt);
